4bdb055045307534f7743d107763d8f79e2115ea
Laura <l.dehours@dimmbizz.com>
2022-12-04 15:25:10 +0100

    Initialisation lmj

---
+++ la-maison-jungle/src/components/App.js
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner";
+import Cart from "./Cart";
+
+function App() {
+  return (
+    <div>
+      <Banner />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
